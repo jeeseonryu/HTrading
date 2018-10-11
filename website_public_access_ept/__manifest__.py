165b478a9cae852aef3d80c83551df365cb825d8
emiprogithub <emiprogithub>
2018-10-11 15:21:58 +0530

    website public access module added

---
+++ website_public_access_ept/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Public access ',
+    'version': '1.0', 
+    'author':'Emipro Technologies',
+    'category': 'Website',
+    'website': ' www.emiprotechnologies.com',
+    'description': "",
+    'depends': ['website'],
+    'data': [
+        'templates/template.xml',
+        'templates/assets.xml'
+    ],  
+    'installable': True,
+    'application': True,
+}
---
+++ website_public_access_ept/static/src/js/login.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+    $(".oe_website_login_container").parents("body").css("overflow","hidden");
+    $(".oe_website_login_container").parents("main").siblings("footer").css("display","none");
+    $(".oe_website_login_container").parents("main").siblings("header").css("display","none");
+})
