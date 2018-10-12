7877edf2b38d3f4d2db613851c4aaec05eb09a0e
emiprogithub <emiprogithub>
2018-10-12 12:12:19 +0530

    new method implemented for login

---
+++ website_public_access_ept/model/ir_http.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models
+from odoo.http import request
+import werkzeug.utils
+from odoo import http
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    @classmethod
+    def _dispatch(cls):   
+        url=request.httprequest.path
+        if not request.session['login'] and url.find('/web/login')==-1 :
+            if url.find('/web/')==-1 and url.find('/website/lang/')==-1  and url.find('/website/translations')==-1:
+                _logger.info("\n****** website public access url:_displtch method of ir.http for url %s"%url)
+                redirect = '/web/login'
+                return http.redirect_with_hash(redirect)
+        return super(IrHttp, cls)._dispatch()
--- website_public_access_ept/static/src/js/login.js
+++ website_public_access_ept/static/src/js/login.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
     $(".oe_website_login_container").parents("body").css("overflow","hidden");
-    $(".oe_website_login_container").parents("main").siblings("footer").css("display","none");
-    $(".oe_website_login_container").parents("main").siblings("header").css("display","none");
+    $(".oe_website_login_container").parents("main").siblings("footer").remove();
+    $(".oe_website_login_container").parents("main").siblings("header").remove();
+//    $(".access_not_allow").parents("main").siblings("footer").remove();
+//    $(".access_not_allow").parents("main").siblings("header").remove();
+//    $(".access_not_allow").parents("main").siblings("div").addClass("hide_when_logout");
+    
 })
